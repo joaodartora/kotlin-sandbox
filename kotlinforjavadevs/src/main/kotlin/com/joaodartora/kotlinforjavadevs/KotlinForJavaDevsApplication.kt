6d2e2b6e123a1715b2ab0b5aca2fae9955385355
João Dartora <joao.dartora@gmail.com>
2021-10-30 13:21:53 -0300

    feat: add base project to Kotlin For Java Developers Course

---
+++ kotlinforjavadevs/src/main/kotlin/com/joaodartora/kotlinforjavadevs/KotlinForJavaDevsApplication.kt
@@ -0,0 +1,11 @@
+package com.joaodartora.kotlinforjavadevs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinForJavaDevsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinForJavaDevsApplication>(*args)
+}
---
+++ kotlinforjavadevs/src/test/kotlin/com/joaodartora/kotlinforjavadevs/KotlinForJavaDevsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joaodartora.kotlinforjavadevs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinForJavaDevsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
